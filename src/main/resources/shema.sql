bddd637c0a9d55cbf5cd97e66d8d8d1ea16000db
moksha-395 <mokshagna395@gmail.com>
2025-12-23 22:19:14 +0530

    Initial commit - Student Management System

--- src/main/resources/shema.sql
+++ src/main/resources/shema.sql
@@ -1,17 +1,6 @@
-SET DATABASE COLLATION "English" STRENGTH IDENTICAL;
-SET IGNORECASE TRUE;
-SET MODE MySQL;
-
-
 CREATE TABLE IF NOT EXISTS student (
-	id INT AUTO_INCREMENT PRIMARY KEY,
-	name VARCHAR(100) NOT NULL,
-	email VARCHAR(100) UNIQUE NOT NULL,
-	age INT NOT NULL
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(50),
+    last_name VARCHAR(50),
+    email VARCHAR(100)
 );
-
-INSERT INTO student(name, email, age) VALUES
-("Amit Sharma","amit@gmail.com", 22),
-("Priya Singh", "priya@gmail.com", 20);
-
-
